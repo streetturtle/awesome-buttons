97dd4b045502f8a9d538dc8a05bc299b91b6ecda
Pavel Makhov <pavel.makhov@behavox.com>
2020-09-01 21:54:27 -0400

    init commit

---
+++ awesome-buttons.lua
@@ -0,0 +1,195 @@
+local wibox = require("wibox")
+local gears = require("gears")
+local naughty = require("naughty")
+
+local buttons = {}
+
+buttons.with_icon = function(args)
+    local type = args.type or 'basic'
+    local color = args.color or '#D8DEE9'
+    local icon = args.icon
+    local shape = args.shape
+    local onclick = args.onclick or function () end
+
+    local result = wibox.widget{
+        {
+            {
+                image = icon,
+                resize = true,
+                forced_height = 20,
+                forced_width = 20,
+                widget = wibox.widget.imagebox
+            },
+            margins = 8,
+            widget = wibox.container.margin
+        },
+        bg = '#00000000',
+        widget = wibox.container.background
+    }
+
+    if type == 'outline' then
+        result:set_shape_border_color(color)
+        result:set_shape_border_width(1)
+    elseif type == 'flat' then
+        result:set_bg(color)
+    end
+
+    if shape == 'circle' then
+        result:set_shape(gears.shape.circle)
+    elseif shape == 'rounded_rect' then
+        result:set_shape(function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end)
+    end
+
+    local old_cursor, old_wibox
+    result:connect_signal("mouse::enter", function(c)
+        if type ~= 'flat' then
+            c:set_bg(color)
+        end
+        local wb = mouse.current_wibox
+        old_cursor, old_wibox = wb.cursor, wb
+        wb.cursor = "hand1"
+    end)
+    result:connect_signal("mouse::leave", function(c)
+        if type ~= 'flat' then
+            c:set_bg('#00000000')
+        end
+        if old_wibox then
+            old_wibox.cursor = old_cursor
+            old_wibox = nil
+        end
+    end)
+
+    result:connect_signal("button::press", function() onclick() end)
+
+    return result
+end
+
+
+buttons.with_text = function(args)
+    local type = args.type or 'basic'
+    local text = args.text
+    local onclick = args.onclick or function(c) naughty.notify{text = 'Button pressed!'} end
+    local color = args.color or '#D8DEE9'
+    local text_size = args.text_size or 10
+
+
+
+    local result = wibox.widget{
+        {
+            {
+                markup = '<span size="' .. text_size .. '000" foreground="' .. ((type == 'flat') and '#00000000' or color) .. '">' .. text ..'</span>',
+                widget = wibox.widget.textbox
+            },
+            top = 4, bottom = 4, left = 8, right = 8,
+            widget = wibox.container.margin
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+
+    }
+
+    if type == 'outline' then
+        result:set_shape_border_color(color)
+        result:set_shape_border_width(1)
+    elseif type == 'flat' then
+        result:set_bg(color)
+    end
+
+    local old_cursor, old_wibox
+    result:connect_signal("mouse::enter", function(c)
+        if type ~= 'flat' then
+            c:set_bg("#00000066")
+        end
+        local wb = mouse.current_wibox
+        old_cursor, old_wibox = wb.cursor, wb
+        wb.cursor = "hand1"
+    end)
+    result:connect_signal("mouse::leave", function(c)
+        if type ~= 'flat' then
+            c:set_bg('#00000000')
+        end
+        if old_wibox then
+            old_wibox.cursor = old_cursor
+            old_wibox = nil
+        end
+    end)
+
+    result:connect_signal("button::press", function() onclick() end)
+
+    return result
+end
+
+
+buttons.with_icon_and_text = function(args)
+
+    local type = args.type or 'basic'
+    local text = args.text
+    local icon = args.icon
+    local onclick = args.onclick or function(c) naughty.notify{text = 'Button pressed!'} end
+    local color = args.color or '#D8DEE9'
+    local text_size = args.text_size or 10
+    local shape = args.shape or 'rounded_rect'
+
+    local result = wibox.widget{
+        {
+            {
+                {
+                    image = icon,
+                    resize = true,
+                    forced_height = 20,
+                    widget = wibox.widget.imagebox
+                },
+                margins = 4,
+                widget = wibox.container.margin
+            },
+            {
+                {
+                    markup = '<span size="' .. text_size .. '000" foreground="' .. ((type == 'flat') and '#00000000' or color) .. '">' .. text ..'</span>',
+                    widget = wibox.widget.textbox
+                },
+                top = 4, bottom = 4, right = 8,
+                widget = wibox.container.margin
+            },
+            layout = wibox.layout.align.horizontal
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+    }
+
+        if type == 'outline' then
+            result:set_shape_border_color(color)
+            result:set_shape_border_width(1)
+        end
+
+        if type == 'flat' then
+            result:set_bg(color)
+        end
+
+        local old_cursor, old_wibox
+        result:connect_signal("mouse::enter", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000044')
+            end
+            local wb = mouse.current_wibox
+            old_cursor, old_wibox = wb.cursor, wb
+            wb.cursor = "hand1"
+        end)
+        result:connect_signal("mouse::leave", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000000')
+            end
+            if old_wibox then
+                old_wibox.cursor = old_cursor
+                old_wibox = nil
+            end
+        end)
+
+        result:connect_signal("button::press", function() onclick() end)
+
+        return result
+
+end
+
+return buttons
